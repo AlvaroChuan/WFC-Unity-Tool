70e06967335606e2f273f81034887b558aa42147
Álvaro <alvarochuan@gmail.com>
2026-02-12 15:09:51 +0100

    Preprocessing WIP

---
+++ Assets/Bitmask/Tileset.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tileset : ScriptableObject
+{
+    [SerializeField] public List<Tile3D> tiles = new List<Tile3D>();
+    [SerializeField] public float tileSize = 1;
+    [SerializeField] public int tileCount = 0;
+    [SerializeField] public List<string> tileTypes = new List<string>();
+    [SerializeField] public List<string> socketTypes = new List<string>();
+}
